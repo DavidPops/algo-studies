2347dcf51a88090a6e67319a040492de5a46f09c
David Popoola <davieedpopoola@gmail.com>
2020-05-03 14:00:18 +0100

    Editorial Solutions to day 1 of Leetcode's 30 days challenge in April 'Single Number'. My solution was implemented in python using Bit Manipulation to use O(1) memory

---
+++ src/leetcode/AprilThirtyDays/SingleNumber.java
@@ -0,0 +1,60 @@
+package leetcode.AprilThirtyDays;
+
+import java.util.*;
+
+class ListOperationSolution {
+    public int singleNumber(int[] nums) {
+        List<Integer> no_duplicate_list = new ArrayList<>();
+
+        for (int i : nums) {
+            if (!no_duplicate_list.contains(i)) {
+                no_duplicate_list.add(i);
+            } else {
+                no_duplicate_list.remove(new Integer(i));
+            }
+        }
+        return no_duplicate_list.get(0);
+    }
+}
+
+class HashTableSolution {
+    public int singleNumber(int[] nums) {
+        HashMap<Integer, Integer> hash_table = new HashMap<>();
+
+        for (int i : nums) {
+            hash_table.put(i, hash_table.getOrDefault(i, 0) + 1);
+        }
+        for (int i : nums) {
+            if (hash_table.get(i) == 1) {
+                return i;
+            }
+        }
+        return 0;
+    }
+}
+
+class MathSolution {
+    public int singleNumber(int[] nums) {
+        int sumOfSet = 0, sumOfNums = 0;
+        Set<Integer> set = new HashSet();
+
+        for (int num : nums) {
+            if (!set.contains(num)) {
+                set.add(num);
+                sumOfSet += num;
+            }
+            sumOfNums += num;
+        }
+        return 2 * sumOfSet - sumOfNums;
+    }
+}
+
+class BitManipulationSolution {
+    public int singleNumber(int[] nums) {
+        int a = 0;
+        for (int i : nums) {
+            a ^= i;
+        }
+        return a;
+    }
+}
