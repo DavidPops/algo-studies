b1d6cfc28b4bfc0126433ef5f4ec69d0c0bcc02f
David Popoola <davieedpopoola@gmail.com>
2020-05-05 13:29:01 +0100

    Solved Group Anagrams and Longest Substring Without Repeating Characters

--- src/leetcode/MayThirtyOneDays/FirstBadVersion.java
+++ src/leetcode/MayThirtyOneDays/FirstBadVersion.java
@@ -1,11 +1,14 @@
 package leetcode.MayThirtyOneDays;
 
+//https://en.wikipedia.org/wiki/Binary_search_algorithm#Implementation_issues
+//http://www.cs.cornell.edu/courses/cs211/2006sp/Lectures/L06-Induction/binary_search.html
+
 public class FirstBadVersion {
     public int firstBadVersion(int n) {
         int left = 1;
         int right = n;
         while (left < right) {
-            int mid = left + (right - left) / 2;
+            int mid = left + (right - left) / 2; // beware of Integer Overflow
             if (isBadVersion(mid)) {
                 right = mid;
             } else {
